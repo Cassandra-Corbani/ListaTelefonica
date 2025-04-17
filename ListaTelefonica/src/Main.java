dfd1758eea0dbc901b30b48067d2c84699b4de70
CASSANDRA RAFAELA CORBANI <cassandra.corbani@alunos.sc.senac.br>
2025-04-16 21:23:44 -0300

    atualizando e corrigindo codigo

--- ListaTelefonica/src/Main.java
+++ ListaTelefonica/src/Main.java
@@ -5,13 +5,9 @@ public class Main {
     public static void main(String[] args) {
         boolean estaAtivo = true;
         String entrada;
-
         Scanner s = new Scanner(System.in);
-
-        // MATRIZ com nome e telefone
         String[][] listaTelefonica = new String[100][2];
         int totalContatos = 0;
-
         while (estaAtivo) {
             System.out.println("Digite o que deseja:\n" +
                     "1 - Adicionar contato\n" +
@@ -20,10 +16,9 @@ public class Main {
                     "4 - Remover contatos\n" +
                     "x - Sair");
             entrada = s.nextLine();
-
             switch (entrada) {
                 case "1":
-                    // Loop para adicionar contatos até que o usuário não queira mais
+
                     boolean querContinuar = true;
                     while (querContinuar && totalContatos < listaTelefonica.length) {
                         System.out.println("Digite o nome do contato:");
@@ -32,14 +27,13 @@ public class Main {
                         System.out.println("Digite o telefone:");
                         String telefone = s.nextLine();
 
-                        // Adiciona o contato na matriz
+
                         listaTelefonica[totalContatos][0] = nome;
                         listaTelefonica[totalContatos][1] = telefone;
                         totalContatos++;
 
                         System.out.println("Contato adicionado com sucesso!");
 
-                        // Pergunta se deseja adicionar outro
                         System.out.println("Deseja adicionar outro contato? (s/n)");
                         String resposta = s.nextLine();
 
@@ -48,14 +42,11 @@ public class Main {
                         }
                     }
 
-                    // Verifica se a agenda está cheia
                     if (totalContatos == listaTelefonica.length) {
                         System.out.println("A agenda está cheia.");
                     }
 
                     break;
-
-                // Coloque outras opções do menu aqui (como buscar, listar, remover)
 
                 case "x":
                     estaAtivo = false;
